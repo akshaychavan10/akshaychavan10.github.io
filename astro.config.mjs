f2f048a8c7728a1beb82c47fe8007b147e9e9103
Akshay Chavan <37307225+akshaychavan10@users.noreply.github.com>
2025-02-16 19:34:44 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  
+  site: 'https://akshaychavan10.github.io
   integrations: [tailwind(), mdx()],
   markdown: {
     shikiConfig: {
